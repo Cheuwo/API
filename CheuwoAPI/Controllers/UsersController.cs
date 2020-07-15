387460e013895dab72cb141d59ec138b7705275c
Gabrielius Laurinavičius <gabrielius9915@gmail.com>
2020-07-16 00:53:34 +0300

    Add User controller

---
+++ CheuwoAPI/Controllers/UsersController.cs
@@ -0,0 +1,74 @@
+﻿using CheuwoAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheuwoAPI.Controllers
+{
+    [Route("api/v1/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class UsersController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+        private List<User> appUsers = new List<User>
+        {
+            new User { UserName = "admin", Password = "1234" },
+            new User { UserName = "user", Password = "1234" }
+        };
+
+        public UsersController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login([FromBody]User login)
+        {
+            IActionResult response = Unauthorized();
+            User user = AuthenticateUser(login);
+            if (user != null)
+            {
+                var tokenString = GenerateJWTToken(user);
+                response = Ok(new
+                {
+                    token = tokenString
+                });
+            }
+            return response;
+        }
+
+        User AuthenticateUser(User loginCredentials)
+        {
+            User user = appUsers.SingleOrDefault(x => x.UserName == loginCredentials.UserName && x.Password == loginCredentials.Password);
+            return user;
+        }
+        string GenerateJWTToken(User userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt: SecretKey"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userInfo.UserName),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+            var token = new JwtSecurityToken(
+            issuer: _config["Jwt: Issuer"],
+            audience: _config["Jwt: Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddDays(30),
+            signingCredentials: credentials
+            );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
