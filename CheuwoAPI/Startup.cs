591575d60acad3fe171cd633777c8995998d511a
Gabrielius Laurinavičius <gabrielius9915@gmail.com>
2020-07-19 02:28:08 +0300

    Add Identity service

--- CheuwoAPI/Startup.cs
+++ CheuwoAPI/Startup.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Text;
+using CheuwoAPI.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,14 @@ namespace CheuwoAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string strCon = Configuration["ConnectionString"];
+            services.AddDbContext<ApiContext>(options =>
+                options.UseSqlServer(strCon));
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ApiContext>()
+                .AddDefaultTokenProviders();
+
             services.Configure<IdentityOptions>(options =>
             {
                 // Password settings.
