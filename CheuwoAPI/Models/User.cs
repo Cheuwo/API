be83026683ef82c9b12c0cd0e7dfc666bf6111be
Gabrielius Laurinavičius <gabrielius9915@gmail.com>
2020-07-19 00:50:24 +0300

    Add UserDTO

--- CheuwoAPI/Models/User.cs
+++ CheuwoAPI/Models/User.cs
@@ -1,8 +1,9 @@
-﻿namespace CheuwoAPI.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CheuwoAPI.Models
 {
-    public class User
+    public class User : IdentityUser
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
+
     }
 }
